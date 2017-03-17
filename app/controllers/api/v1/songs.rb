e302ed9c4f68ede358593a56eac9290492a330d6
aza <aza@lainventoria.com.ar>
2017-03-17 02:05:10 -0300

    api and specs for songs

--- app/controllers/api/v1/songs.rb
+++ app/controllers/api/v1/songs.rb
@@ -4,29 +4,43 @@ module API
       include API::V1::Defaults
 
       resource :songs do
-        desc "Create a song"
+        before do
+          @song = Song.find(params[:id]) if params[:id]
+        end
+
+        desc 'Create a song'
         params do
           requires :name, type: String, desc: "Name of the song"
+          requires :duration, type: Integer, desc: "Duration of the song"
         end
-        post "", root: :songs do
+        post do
           Song.create!(permitted_params)
         end
 
-        desc "Delete a song"
+        desc 'Get a song'
+        params do
+          requires :id, type: Integer, desc: 'ID of the song'
+        end
+        get ':id' do
+          @song
+        end
+
+        desc 'Delete a song'
         params do
           requires :id, type: Integer, desc: "ID of the song"
         end
-        delete ":id", root: :songs do
-          Song.find(permitted_params[:id]).destroy
+        delete ":id" do
+          @song.destroy
         end
 
         desc "Update an song"
         params do
           requires :id, type: Integer, desc: "ID of the song"
           optional :name, type: String, desc: "Name of the song"
+          optional :duration, type: Integer, desc: "Duration of the song"
         end
-        put ":id", root: :songs do
-          Song.find(permitted_params[:id]).update!(permitted_params)
+        put ":id" do
+          @song.update!(permitted_params)
         end
       end
     end
